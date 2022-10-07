0f9f4bd2a2c9b44a4f591846af208d8e9e1353d6
Anirban Basak <jeetbasak2002@gmail.com>
2022-10-07 17:00:22 +0530

    07/10/2022

--- app/src/main/java/com/basakjeet08/quizapp/MainActivity.kt
+++ app/src/main/java/com/basakjeet08/quizapp/MainActivity.kt
@@ -1,11 +1,31 @@
 package com.basakjeet08.quizapp
 
-import androidx.appcompat.app.AppCompatActivity
+import android.content.Intent
 import android.os.Bundle
+import android.widget.Toast
+import androidx.appcompat.app.AppCompatActivity
+import com.basakjeet08.quizapp.databinding.ActivityMainBinding
+import com.basakjeet08.quizapp.databinding.ActivityMainBinding.inflate
 
 class MainActivity : AppCompatActivity() {
+
+    private lateinit var binding:ActivityMainBinding
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        binding = inflate(layoutInflater)
+        setContentView(binding.root)
+
+        binding.btnStart.setOnClickListener {
+            val name = binding.etName.text.toString()
+            if(name.isEmpty())
+                Toast.makeText(this,"Enter Your Name" , Toast.LENGTH_SHORT).show()
+            else{
+                val intent = Intent(this,QuizQuestions::class.java)
+                intent.putExtra("user_name" , name)
+                startActivity(intent)
+                finish()
+            }
+        }
     }
 }
