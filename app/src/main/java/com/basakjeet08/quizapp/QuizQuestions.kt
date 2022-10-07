1122b2696f4b87b3591c2587f983fd6bb1872bd7
Anirban Basak <jeetbasak2002@gmail.com>
2022-10-07 16:35:02 +0530

    07/10/2022

---
+++ app/src/main/java/com/basakjeet08/quizapp/QuizQuestions.kt
@@ -0,0 +1,11 @@
+package com.basakjeet08.quizapp
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class QuizQuestions : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_quiz_questions)
+    }
+}
