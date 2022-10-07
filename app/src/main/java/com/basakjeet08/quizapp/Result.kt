7654d172f9af1ebf5209de729a99152f7b7c41e1
Anirban Basak <jeetbasak2002@gmail.com>
2022-10-07 16:50:31 +0530

    07/10/2022

---
+++ app/src/main/java/com/basakjeet08/quizapp/Result.kt
@@ -0,0 +1,11 @@
+package com.basakjeet08.quizapp
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class Result : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_result)
+    }
+}
