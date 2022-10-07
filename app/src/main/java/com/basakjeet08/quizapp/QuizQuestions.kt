a66254ae2869210b6af2724927321fc4130fc53e
Anirban Basak <jeetbasak2002@gmail.com>
2022-10-07 18:02:25 +0530

    07/10/2022

--- app/src/main/java/com/basakjeet08/quizapp/QuizQuestions.kt
+++ app/src/main/java/com/basakjeet08/quizapp/QuizQuestions.kt
@@ -1,6 +1,10 @@
 package com.basakjeet08.quizapp
 
+import android.content.Intent
+import android.graphics.Color
+import android.graphics.Typeface
 import android.os.Bundle
+import android.widget.TextView
 import androidx.appcompat.app.AppCompatActivity
 import com.basakjeet08.quizapp.databinding.ActivityQuizQuestionsBinding
 import com.basakjeet08.quizapp.databinding.ActivityQuizQuestionsBinding.inflate
@@ -9,17 +13,69 @@ class QuizQuestions : AppCompatActivity() {
     private val questionList = Constants.getQuestions()
     private lateinit var binding : ActivityQuizQuestionsBinding
     private var currentQuestion = 0
-
+    private var userAnswer = 0
+    private var score = 0
+    private lateinit var name:String
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         binding = inflate(layoutInflater)
         setContentView(binding.root)
         setQuestion()
+        name = intent.getStringExtra("user_name").toString()
+
+
+
+        binding.tvOptionOne.setOnClickListener {
+            userAnswer = 1
+            setTextViewDefaults()
+            setTextViewSelected(binding.tvOptionOne)
+        }
+        binding.tvOptionTwo.setOnClickListener {
+            userAnswer = 2
+            setTextViewDefaults()
+            setTextViewSelected(binding.tvOptionTwo)
+        }
+        binding.tvOptionThree.setOnClickListener {
+            userAnswer = 3
+            setTextViewDefaults()
+            setTextViewSelected(binding.tvOptionThree)
+        }
+        binding.tvOptionFour.setOnClickListener {
+            userAnswer = 4
+            setTextViewDefaults()
+            setTextViewSelected(binding.tvOptionFour)
+        }
+
+
+
+
+
+
+
+
 
         binding.btnSubmit.setOnClickListener {
-            currentQuestion++
-            setQuestion()
+            if(binding.btnSubmit.text.toString() == "Submit"){
+                showCorrectedAnswer()
+                userAnswer = 0
+                binding.btnSubmit.text = "Next"
+            }
+            else {
+                if(currentQuestion == questionList.size-1){
+                    val intent = Intent(this,Result::class.java)
+                    intent.putExtra("score",score.toString())
+                    intent.putExtra("user_name",name)
+                    startActivity(intent)
+                    finish()
+                }
+                else{
+                    binding.btnSubmit.text = "Submit"
+                    currentQuestion++
+                    setQuestion()
+                }
+
+            }
         }
 
     }
@@ -27,8 +83,8 @@ class QuizQuestions : AppCompatActivity() {
 
 
     private fun setQuestion(){
+        setTextViewDefaults()
         val ques = questionList[currentQuestion]
-
         with(ques){
             binding.tvQuestion.text = question
             binding.ivFlag.setImageResource(image)
@@ -40,4 +96,41 @@ class QuizQuestions : AppCompatActivity() {
         binding.progressBar.progress = currentQuestion
         binding.tvProgress.text = "$currentQuestion/${binding.progressBar.max}"
     }
+
+    private fun setTextViewDefaults(){
+        with(binding){
+            tvOptionOne.setBackgroundResource(R.drawable.default_option_border_bg)
+            tvOptionTwo.setBackgroundResource(R.drawable.default_option_border_bg)
+            tvOptionThree.setBackgroundResource(R.drawable.default_option_border_bg)
+            tvOptionFour.setBackgroundResource(R.drawable.default_option_border_bg)
+
+
+            tvOptionOne.setTypeface(null, Typeface.NORMAL)
+            tvOptionTwo.setTypeface(null, Typeface.NORMAL)
+            tvOptionThree.setTypeface(null, Typeface.NORMAL)
+            tvOptionFour.setTypeface(null, Typeface.NORMAL)
+        }
+    }
+    private fun setTextViewSelected(tv:TextView){
+        tv.setBackgroundResource(R.drawable.selected_option_border_bg)
+        tv.setTypeface(null,Typeface.BOLD)
+    }
+
+    private fun showCorrectedAnswer(){
+        when(userAnswer){
+            1 -> binding.tvOptionOne.setBackgroundColor(Color.parseColor("#B82525"))
+            2 -> binding.tvOptionTwo.setBackgroundColor(Color.parseColor("#B82525"))
+            3 -> binding.tvOptionThree.setBackgroundColor(Color.parseColor("#B82525"))
+            4 -> binding.tvOptionFour.setBackgroundColor(Color.parseColor("#B82525"))
+        }
+        when(questionList[currentQuestion].correctAnswer){
+            1 -> binding.tvOptionOne.setBackgroundColor(Color.parseColor("#47914A"))
+            2 -> binding.tvOptionTwo.setBackgroundColor(Color.parseColor("#47914A"))
+            3 -> binding.tvOptionThree.setBackgroundColor(Color.parseColor("#47914A"))
+            4 -> binding.tvOptionFour.setBackgroundColor(Color.parseColor("#47914A"))
+        }
+        if(userAnswer == questionList[currentQuestion].correctAnswer)
+            score++
+    }
+
 }
--- app/src/main/java/com/basakjeet08/quizapp/Result.kt
+++ app/src/main/java/com/basakjeet08/quizapp/Result.kt
@@ -1,11 +1,21 @@
 package com.basakjeet08.quizapp
 
-import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
+import com.basakjeet08.quizapp.databinding.ActivityResultBinding
+import com.basakjeet08.quizapp.databinding.ActivityResultBinding.inflate
 
 class Result : AppCompatActivity() {
+    private lateinit var binding: ActivityResultBinding
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_result)
+        binding = inflate(layoutInflater)
+        setContentView(binding.root)
+
+        val name = intent.getStringExtra("user_name").toString()
+        val score = intent.getStringExtra("score").toString()
+
+        binding.tvName.text = name
+        binding.tvResult.text = "You Scored $score/10"
     }
 }
